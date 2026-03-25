36a91bd097c56bc966ccf56fa28ec013e6ce707b
ZhuHaoming2005 <m17663905966@163.com>
2026-03-26 00:57:34 +0800

    feat: add file-type worker for Cloudflare compatibility

---
+++ src/types/file-type-core.d.ts
@@ -0,0 +1,19 @@
+declare module 'file-type/core.js' {
+  export type FileTypeResult =
+    | {
+        readonly ext: string
+        readonly mime: string
+      }
+    | undefined
+
+  export class FileTypeParser {}
+
+  export function fileTypeFromBlob(blob: Blob): Promise<FileTypeResult>
+  export function fileTypeFromBuffer(buffer: Uint8Array | ArrayBuffer): Promise<FileTypeResult>
+  export function fileTypeFromStream(stream: ReadableStream<Uint8Array>): Promise<FileTypeResult>
+  export function fileTypeFromTokenizer(tokenizer: unknown): Promise<FileTypeResult>
+
+  export const reasonableDetectionSizeInBytes: number
+  export const supportedExtensions: ReadonlySet<string>
+  export const supportedMimeTypes: ReadonlySet<string>
+}
