a8b650dcb6126f8f8e8170d331fff2fc30a7d464
ZhuHaoming2005 <m17663905966@163.com>
2026-03-29 21:54:55 +0800

    chore: remove agent configuration and script files from project

--- .superpowers/brainstorm/debug-test/state/launch-server.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-set "BRAINSTORM_DIR=d:\code\project\campusblog\.superpowers\brainstorm\debug-test"
-set "BRAINSTORM_HOST=127.0.0.1"
-set "BRAINSTORM_URL_HOST=localhost"
-node "C:\Users\z1766\.codex\superpowers\skills\brainstorming\scripts\server.cjs" 1>>"d:\code\project\campusblog\.superpowers\brainstorm\debug-test\state\server.log" 2>>"d:\code\project\campusblog\.superpowers\brainstorm\debug-test\state\server.err.log"
