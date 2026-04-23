b4eeb149a7422d6a3cb743a4d0c44c9d5fbec6aa
ZhuHaoming2005 <m17663905966@163.com>
2026-04-23 18:19:17 +0800

    Add verification fields to users table in migration 20260423_101813
    - Added `_verified` integer column to track user verification status.
    - Added `_verificationtoken` text column for storing verification tokens.
    - Implemented up and down migration functions for adding and removing these fields.

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20260324_145932 from './20260324_145932';
+import * as migration_20260423_101813_verification_fields from './20260423_101813_verification_fields';
 
 export const migrations = [
   {
     up: migration_20260324_145932.up,
     down: migration_20260324_145932.down,
-    name: '20260324_145932'
+    name: '20260324_145932',
+  },
+  {
+    up: migration_20260423_101813_verification_fields.up,
+    down: migration_20260423_101813_verification_fields.down,
+    name: '20260423_101813_verification_fields'
   },
 ];
